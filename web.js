1a48bf1ea536de8a3581a9c87c181d4cc40736dc
root <root@ip-172-31-11-50.us-west-2.compute.internal>
2013-07-10 04:52:06 +0000

    Otra vez web,js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+	var fs = require('fs');
   var indexContents =fs.readFileSync( 'index.html').toString();
 	
   response.send(indexContents);

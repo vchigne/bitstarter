1111101df9b0c7ea25dec4558abd7ea272936f67
root <root@ip-172-31-11-50.us-west-2.compute.internal>
2013-07-10 04:28:16 +0000

    Otra vez web,js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexContents =fs.readFileSync( 'index.html').toString();
+	
+  response.send(indexContents);
+  response.send('Bunch of crap');
 });
 
 var port = process.env.PORT || 5000;
